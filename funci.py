2199721a7838a6d37a6ec3f3b9e21f751e53bb7d
Giovanny <gioguerra.je@gmail.com>
2025-06-25 22:57:14 +0200

    Funciones V1

--- funci.py
+++ funci.py
@@ -7,3 +7,20 @@ def calcular (num1,num2,op):
         return num1*num2
     elif op ==4:
         return num1/num2
+
+def votar(op,edad):
+    if op==1:
+        if edad>18:
+            return ("Usted Puede Votar")
+        else:
+            return ("Usted No Puede Votar")
+def nota(op,notas):
+    if op==1:
+        if notas>=4:
+            return ("Usted Aprueba")
+        else:
+            return ("Usted Reprueba")
+def stoca(op,i):
+    if op==2:
+        if i["cantidad"]<=0:
+            return ("Este Producto Esta sin stock")
