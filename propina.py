456f63db6201edb811f55fad9b236cb7cb622f54
Giovanny <gioguerra.je@gmail.com>
2025-07-03 22:53:55 +0200

    Usando Max

---
+++ propina.py
@@ -0,0 +1,5 @@
+from funci import propina
+while True:
+    print("Se le dara de propina a mesero, el 20% de el precio de el plato")
+    costo=int(input("Ingrese precio de el plato: "))
+    print(propina(costo))
